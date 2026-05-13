a6e6cc7b035b7dec3b0ce1d0b2241ff80e86e5f2
Sourav <info.sourav174@gmail.com>
2026-05-13 15:03:11 +0530

    meta-data

--- lib/app/helpers/widgets/desktop/contact_section.dart
+++ lib/app/helpers/widgets/desktop/contact_section.dart
@@ -937,13 +937,13 @@ class _ContactField extends StatelessWidget {
     return TextField(
       controller: controller,
       maxLines: maxLines,
-      style: theme.bodyMedium!.copyWith(color: Colors.white),
+      style: theme.bodyMedium!.copyWith(color: Colors.white, fontSize: 12),
       decoration: InputDecoration(
         hintText: hint,
 
         hintStyle: theme.bodyMedium!.copyWith(
           color: Colors.white.withOpacity(0.5),
-          fontSize: 16,
+          fontSize: 12,
         ),
         filled: true,
         fillColor: Colors.white.withOpacity(0.05),
